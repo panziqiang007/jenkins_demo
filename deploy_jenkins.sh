e05382acdae2846bbec799db094815f688523d27
panziqiang007 <qc_bb6c6bc020f84b8ba3338d06d31bdf77@git.code.tencent.com>
2020-03-31 13:49:03 +0800

    remove rm

--- deploy_jenkins.sh
+++ deploy_jenkins.sh
@@ -9,9 +9,9 @@
 #     Last modified on 2018/08/16
 
 # 容器名称
-CONTAINER="jenkins_demo"
+# CONTAINER="jenkins_demo"
 # 镜像名称（以日期时间为镜像标签，防止重复）
-IMAGE=$CONTAINER":"$(date -d "today" +"%Y%m%d_%H%M%S")
+# IMAGE=$CONTAINER":"$(date -d "today" +"%Y%m%d_%H%M%S")
 
 # 删除滚动更新残留的容器
 # docker rm "docker ps -a | grep -w $CONTAINER'_'$CONTAINER | awk '{print $1}'"
@@ -19,7 +19,7 @@ IMAGE=$CONTAINER":"$(date -d "today" +"%Y%m%d_%H%M%S")
 # docker rmi --force "docker images | grep -w $CONTAINER | awk '{print $3}'"
 
 # 创建新镜像
-docker build -t $IMAGE .
+docker build -t jenkins_demo:v1 .
 
 # 删除 docker-compose.jenkins.yml 文件，防止使用相同镜像
 rm -rf docker-compose.jenkins.yml
@@ -28,7 +28,7 @@ rm -rf docker-compose.jenkins.yml
 cp docker-compose.src.yml docker-compose.jenkins.yml
 
 # 替换镜像名标志位为最新镜像
-sed -i s%IMAGE_LATEST%$IMAGE%g docker-compose.jenkins.yml
+# sed -i jenkins_demo:v1 docker-compose.jenkins.yml
 
 # 使用 docker stack 启动服务
-docker stack deploy -c docker-compose.jenkins.yml $CONTAINER
+docker stack deploy -c docker-compose.jenkins.yml jenkins_demo
